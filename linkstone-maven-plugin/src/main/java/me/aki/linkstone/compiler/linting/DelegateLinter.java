f852586e62f4262974e378a6d92b259e3278100f
aki_ks <aki@kaysubs.de>
2018-07-06 07:44:25 +0200

    Alow only one delegate field and require constructor

--- linkstone-maven-plugin/src/main/java/me/aki/linkstone/compiler/linting/DelegateLinter.java
+++ linkstone-maven-plugin/src/main/java/me/aki/linkstone/compiler/linting/DelegateLinter.java
@@ -6,6 +6,7 @@ import me.aki.linkstone.compiler.meta.DelegateMeta;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
@@ -28,22 +29,34 @@ public class DelegateLinter implements Linter {
         for (ClassNode cn : classes) {
             Set<Type> delegateInterfaces = new HashSet<>();
 
+            int delegateCount = 0;
             for (FieldNode fn : cn.fields) {
                 DelegateMeta meta = DelegateMeta.from(fn);
                 if (!meta.hasAnnotation()) {
                     continue;
                 }
 
+                if(delegateCount++ >= 1) {
+                    ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
+                    String message = "Multiple @Delegate fields found";
+                    report.addError(new ErrorReport.Error(message, location));
+                }
+
                 for (Type t : meta.getDelegateClasses()) {
                     if (t.getSort() == Type.OBJECT) {
                         ClassNode tcn = classStore.getClass(t.getInternalName());
-                        if (!Modifier.isInterface(tcn.access)) {
-                            ErrorReport.Class location = new ErrorReport.Class(cn.name);
+
+                        if(tcn == null) {
+                            ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
+                            String message = "Could not find class " + t.getInternalName() + " in dependency artifacts";
+                            report.addError(new ErrorReport.Error(message, location));
+                        } else if (!Modifier.isInterface(tcn.access)) {
+                            ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
                             String message = "@Delegate annotation contains a non interface type";
                             report.addError(new ErrorReport.Error(message, location));
                         }
                     } else {
-                        ErrorReport.Class location = new ErrorReport.Class(cn.name);
+                        ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
                         String message = "@Delegate annotation contains a non class type";
                         report.addError(new ErrorReport.Error(message, location));
                     }
@@ -51,12 +64,28 @@ public class DelegateLinter implements Linter {
 
                 for (Type t : meta.getDelegateClasses()) {
                     if (!delegateInterfaces.add(t)) {
-                        ErrorReport.Class location = new ErrorReport.Class(cn.name);
-                        String message = "multiple @Delegate annotations contain the same type (" + t.getClassName() + ")";
+                        ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
+                        String message = "Multiple @Delegate annotations contain the same type (" + t.getClassName() + ")";
                         report.addError(new ErrorReport.Error(message, location));
                     }
                 }
+
+                MethodNode constructor = findConstructor(cn, fn);
+                if (constructor == null) {
+                    ErrorReport.Field location = new ErrorReport.Field(cn.name, fn.name, fn.desc);
+                    String message = "Could not find constructor corresponding constructor for delegate field";
+                    report.addError(new ErrorReport.Error(message, location));
+                }
             }
         }
+    }
+
+    private MethodNode findConstructor(ClassNode cn, FieldNode fn) {
+        for(MethodNode mn : cn.methods) {
+            if(mn.name.equals("<init>") && mn.desc.equals("(" + fn.desc + ")V")) {
+                return mn;
+            }
+        }
+        return null;
     }
 }

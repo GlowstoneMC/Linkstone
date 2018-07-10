70236101bb5caca9bfbce80bf7aa9613dd73e588
aki_ks <aki@kaysubs.de>
2018-07-10 21:52:40 +0200

    Don't replace field accesses in accessors
    If a field getter or setter accessed its own field,
    it was replaced against an invoke of itself.
    This will no longer occur.

--- linkstone-runtime/src/main/java/net/glowstone/linkstone/runtime/direct/DirectFieldAccessReplaceVisitor.java
+++ linkstone-runtime/src/main/java/net/glowstone/linkstone/runtime/direct/DirectFieldAccessReplaceVisitor.java
@@ -12,7 +12,11 @@ import static org.objectweb.asm.Opcodes.*;
  * against invokes of the corresponding getters and setters.
  */
 public class DirectFieldAccessReplaceVisitor extends ClassVisitor {
+    private static final String GETTER_PREFIX = "$linkstone$getter$";
+    private static final String SETTER_PREFIX = "$linkstone$setter$";
+
     private final FieldSet fieldsToReplace;
+    private String classname;
 
     /**
      * @param fieldsToReplace all fields whose accesses will be replaced
@@ -20,6 +24,12 @@ public class DirectFieldAccessReplaceVisitor extends ClassVisitor {
      */
     public DirectFieldAccessReplaceVisitor(FieldSet fieldsToReplace) {
         this(fieldsToReplace, null);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        super.visit(version, access, name, signature, superName, interfaces);
+        this.classname = name;
     }
 
     /**
@@ -32,34 +42,76 @@ public class DirectFieldAccessReplaceVisitor extends ClassVisitor {
     }
 
     @Override
-    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-        MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+    public MethodVisitor visitMethod(int mAccess, String mName, String mDescriptor, String mSignature, String[] mExceptions) {
+        MethodVisitor mv = super.visitMethod(mAccess, mName, mDescriptor, mSignature, mExceptions);
         mv = new MethodVisitor(api, mv) {
             @Override
-            public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-                if (fieldsToReplace.contains(owner, name, descriptor)) {
+            public void visitFieldInsn(int opcode, String fOwner, String fName, String fDescriptor) {
+                if (fieldsToReplace.contains(fOwner, fName, fDescriptor)) {
+                    AccessorType accessor = isSelfAccessor(classname, mName, mDescriptor, fOwner, fName, fDescriptor);
+
                     switch (opcode) {
                         case GETFIELD:
                         case GETSTATIC: {
+                            if (accessor == AccessorType.GETTER) {
+                                break;
+                            }
+
                             int invokeOpcode = opcode == GETSTATIC ? INVOKESTATIC : INVOKEVIRTUAL;
-                            String methodDescriptor = Type.getMethodDescriptor(Type.getType(descriptor));
-                            super.visitMethodInsn(invokeOpcode, owner, "$linkstone$getter$" + name, methodDescriptor, false);
-                            break;
+                            String methodDescriptor = Type.getMethodDescriptor(Type.getType(fDescriptor));
+                            super.visitMethodInsn(invokeOpcode, fOwner, GETTER_PREFIX + fName, methodDescriptor, false);
+                            return;
                         }
 
                         case PUTFIELD:
                         case PUTSTATIC: {
+                            if (accessor == AccessorType.SETTER) {
+                                break;
+                            }
+
                             int invokeOpcode = opcode == PUTSTATIC ? INVOKESTATIC : INVOKEVIRTUAL;
-                            String methodDescriptor = Type.getMethodDescriptor(Type.VOID_TYPE, Type.getType(descriptor));
-                            super.visitMethodInsn(invokeOpcode, owner, "$linkstone$setter$" + name, methodDescriptor, false);
-                            break;
+                            String methodDescriptor = Type.getMethodDescriptor(Type.VOID_TYPE, Type.getType(fDescriptor));
+                            super.visitMethodInsn(invokeOpcode, fOwner, SETTER_PREFIX + fName, methodDescriptor, false);
+                            return;
                         }
                     }
-                } else {
-                    super.visitFieldInsn(opcode, owner, name, descriptor);
                 }
+
+                super.visitFieldInsn(opcode, fOwner, fName, fDescriptor);
             }
         };
         return mv;
+    }
+
+    /**
+     * Check whether a method is a getter or setter for a field.
+     *
+     * @param mOwner class containing method
+     * @param mName name of method
+     * @param mDesc descriptor of method
+     * @param fOwner class containing field
+     * @param fName name of field
+     * @param fDesc descriptor of field
+     * @return whether the method is a getter or setter for the field.
+     */
+    private AccessorType isSelfAccessor(String mOwner, String mName, String mDesc,
+                                        String fOwner, String fName, String fDesc) {
+        if (mOwner.equals(fOwner)) {
+            boolean isGetter = mName.equals(GETTER_PREFIX + fName) && mDesc.equals("()" + fDesc);
+            if (isGetter) {
+                return AccessorType.GETTER;
+            }
+
+            boolean isSetter = mName.equals(SETTER_PREFIX + fName) && mDesc.equals("(" + fDesc + ")V");
+            if (isSetter) {
+                return AccessorType.SETTER;
+            }
+        }
+
+        return null;
+    }
+
+    enum AccessorType {
+        GETTER, SETTER;
     }
 }

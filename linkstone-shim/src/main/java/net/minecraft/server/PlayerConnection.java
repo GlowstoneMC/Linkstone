a8f9afc24ffc85d12af4dd298fae06c0bf83a55c
aki_ks <aki@kaysubs.de>
2018-07-31 23:43:43 +0200

    Migrate PlayerConnection

--- linkstone-shim/src/main/java/net/minecraft/server/PlayerConnection.java
+++ linkstone-shim/src/main/java/net/minecraft/server/PlayerConnection.java
@@ -1,15 +1,20 @@
 package net.minecraft.server;
 
-import net.glowstone.linkstone.PacketUtils;
 import net.glowstone.linkstone.annotations.LClassfile;
+import net.glowstone.linkstone.annotations.LField;
+import net.glowstone.linkstone.annotations.LGenerate;
+import net.glowstone.linkstone.annotations.LMethod;
 
 import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
 
 @LClassfile(version = V1_12_R1)
 public class PlayerConnection {
+    @LGenerate
+    @LField(version = V1_12_R1)
     public EntityPlayer player;
 
+    @LMethod(version = V1_12_R1)
     public void sendPacket(final Packet<?> packet) {
-        player.glowPlayer.getSession().send(PacketUtils.packetToMessage(packet));
+        player.glowPlayer.getSession().send(packet.toGlowMessage());
     }
 }

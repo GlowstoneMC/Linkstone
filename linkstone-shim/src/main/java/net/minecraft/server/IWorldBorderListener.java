765c7bbf7cbd6cbf19b433fd3e5b919e11617ea4
aki_ks <aki@kaysubs.de>
2018-08-01 02:49:20 +0200

    Migrate IWorldBorderListener

--- linkstone-shim/src/main/java/net/minecraft/server/IWorldBorderListener.java
+++ linkstone-shim/src/main/java/net/minecraft/server/IWorldBorderListener.java
@@ -1,24 +1,31 @@
 package net.minecraft.server;
 
 import net.glowstone.linkstone.annotations.LClassfile;
+import net.glowstone.linkstone.annotations.LMethod;
 
 import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
 
 @LClassfile(version = V1_12_R1)
 public interface IWorldBorderListener {
+    @LMethod(version = V1_12_R1, name = "a")
+    void onSizeChange(WorldBorder border, double size);
 
-    public void a(WorldBorder var1, double var2);
+    @LMethod(version = V1_12_R1, name = "a")
+    void onTransition(WorldBorder border, double oldSize, double newSize, long time);
 
-    public void a(WorldBorder var1, double var2, double var4, long var6);
+    @LMethod(version = V1_12_R1, name = "a")
+    void onCenterRelocated(WorldBorder border, double x, double z);
 
-    public void a(WorldBorder var1, double var2, double var4);
+    @LMethod(version = V1_12_R1, name = "a")
+    void onWarnTimeChange(WorldBorder border, int time);
 
-    public void a(WorldBorder var1, int var2);
+    @LMethod(version = V1_12_R1, name = "b")
+    void onWarnDistanceChange(WorldBorder border, int distance);
 
-    public void b(WorldBorder var1, int var2);
+    @LMethod(version = V1_12_R1, name = "b")
+    void onDamageChange(WorldBorder border, double newDamage);
 
-    public void b(WorldBorder var1, double var2);
-
-    public void c(WorldBorder var1, double var2);
-
+    //TODO @Isaiah Deobfuscate this method
+    @LMethod(version = V1_12_R1, name = "c")
+    void c(WorldBorder border, double var2);
 }

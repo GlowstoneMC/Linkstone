7ece477549479c8e5f8550b18ecf06c1c6740b91
aki_ks <aki@kaysubs.de>
2018-08-01 03:20:43 +0200

    Migrate EnumDifficulty

--- linkstone-shim/src/main/java/net/minecraft/server/EnumDifficulty.java
+++ linkstone-shim/src/main/java/net/minecraft/server/EnumDifficulty.java
@@ -1,6 +1,9 @@
 package net.minecraft.server;
 
 import net.glowstone.linkstone.annotations.LClassfile;
+import net.glowstone.linkstone.annotations.LField;
+import net.glowstone.linkstone.annotations.LGenerate;
+import net.glowstone.linkstone.annotations.LMethod;
 
 import java.util.Locale;
 
@@ -8,23 +11,37 @@ import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
 
 @LClassfile(version = V1_12_R1)
 public enum EnumDifficulty {
-    PEACEFUL, EASY, NORMAL, HARD;
+    PEACEFUL(0, "peaceful"), EASY(1, "easy"), NORMAL(2, "normal"), HARD(3, "hard");
 
-    private static final EnumDifficulty[] e;
-
-    public int a() {
-        return this.ordinal(); // f
-    }
-
+    @LMethod(version = V1_12_R1)
     public static EnumDifficulty getById(int n) {
-        return e[n % e.length];
+        return values[n % values.length];
     }
 
-    public String b() {
-        return "options.difficulty." + this.name().toLowerCase(Locale.ENGLISH); // g 
+    @LGenerate
+    @LField(version = V1_12_R1, name = "e")
+    private static final EnumDifficulty[] values = EnumDifficulty.values();
+
+    @LGenerate
+    @LField(version = V1_12_R1, name = "f")
+    private final int id;
+
+    @LGenerate
+    @LField(version = V1_12_R1, name = "g")
+    private final String optionName;
+
+    EnumDifficulty(int id, String name) {
+        this.id = id;
+        this.optionName = "options.difficulty." + name;
     }
 
-    static {
-        e = EnumDifficulty.values();
+    @LMethod(version = V1_12_R1, name = "a")
+    public int getId() {
+        return id;
+    }
+
+    @LMethod(version = V1_12_R1, name = "b")
+    public String getOptionName() {
+        return optionName;
     }
 }

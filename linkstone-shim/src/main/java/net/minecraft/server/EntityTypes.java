f960a6d48b79771df80e1e9c535b7ccffc347da4
aki_ks <aki@kaysubs.de>
2018-08-01 03:25:22 +0200

    Add note for Isaiah to EntityTypes

--- linkstone-shim/src/main/java/net/minecraft/server/EntityTypes.java
+++ linkstone-shim/src/main/java/net/minecraft/server/EntityTypes.java
@@ -6,7 +6,7 @@ import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
 
 @LClassfile(version = V1_12_R1)
 public class EntityTypes {
-
+    //TODO @Isaiah Deobfuscate, Annotated Method this field (and initialize the map?)
     public static final RegistryMaterials<MinecraftKey, Class<? extends Entity>> b = new RegistryMaterials<>();
 
 }

3cafdad10731ba0974114190c74505315858a732
aki_ks <aki@kaysubs.de>
2018-08-21 06:00:03 +0200

    Implement PacketPlayOutCustomPayload

---
+++ linkstone-shim/src/main/java/net/minecraft/server/PacketPlayOutCustomPayload.java
@@ -0,0 +1,35 @@
+package net.minecraft.server;
+
+import com.flowpowered.network.Message;
+import net.glowstone.linkstone.annotations.LClassfile;
+import net.glowstone.linkstone.annotations.LConstructor;
+import net.glowstone.linkstone.annotations.LField;
+import net.glowstone.linkstone.annotations.LGenerate;
+import net.glowstone.net.message.play.game.PluginMessage;
+
+import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
+
+@LClassfile(version = V1_12_R1)
+public class PacketPlayOutCustomPayload implements Packet<PacketListenerPlayOut> {
+    @LGenerate
+    @LField(version = V1_12_R1, name = "a")
+    private String channel;
+
+    @LGenerate
+    @LField(version = V1_12_R1, name = "b")
+    private PacketDataSerializer data;
+
+    @LConstructor(version = V1_12_R1)
+    public PacketPlayOutCustomPayload() {}
+
+    @LConstructor(version = V1_12_R1)
+    public PacketPlayOutCustomPayload(String channel, PacketDataSerializer data) {
+        this.channel = channel;
+        this.data = data;
+    }
+
+    @Override
+    public Message toGlowMessage() {
+        return new PluginMessage(channel, data.toByteArray());
+    }
+}

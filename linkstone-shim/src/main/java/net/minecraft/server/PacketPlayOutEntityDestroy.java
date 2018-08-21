bee5d4e1488ae620de649545a43bdca4b01cb472
aki_ks <aki@kaysubs.de>
2018-08-21 05:25:29 +0200

    Implement PacketPlayOutEntityDestroy

---
+++ linkstone-shim/src/main/java/net/minecraft/server/PacketPlayOutEntityDestroy.java
@@ -0,0 +1,32 @@
+package net.minecraft.server;
+
+import com.flowpowered.network.Message;
+import net.glowstone.linkstone.annotations.LClassfile;
+import net.glowstone.linkstone.annotations.LConstructor;
+import net.glowstone.linkstone.annotations.LField;
+import net.glowstone.linkstone.annotations.LGenerate;
+import net.glowstone.net.message.play.entity.DestroyEntitiesMessage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static net.glowstone.linkstone.annotations.Version.V1_12_R1;
+
+@LClassfile(version = V1_12_R1)
+public class PacketPlayOutEntityDestroy implements Packet<PacketListenerPlayOut> {
+    @LGenerate
+    @LField(version = V1_12_R1, name = "a")
+    private int[] entityIds;
+
+    @LConstructor(version = V1_12_R1)
+    public PacketPlayOutEntityDestroy(int[] entityIds) {
+        this.entityIds = entityIds;
+    }
+
+    @Override
+    public Message toGlowMessage() {
+        List<Integer> entityIdList = Arrays.stream(entityIds).boxed().collect(Collectors.toList());
+        return new DestroyEntitiesMessage(entityIdList);
+    }
+}

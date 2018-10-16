427c271e65800ef34e9eff748ed4c2d718c0c2d0
mastercoms <mastercoms@tuta.io>
2018-10-16 13:32:25 -0400

    Add build-external.sh and update Glowkit version

---
+++ build-external.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir ext
+cd ext
+curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar" -o BuildTools.jar
+java -jar BuildTools.jar
+cd ..
+cp ext/craftbukkit-1.12.2.jar templates/1_12_R1.jar

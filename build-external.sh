eda33f394c2ddc5e71a38a9a28b42ef08f2a02cd
mastercoms <mastercoms@tuta.io>
2021-01-16 14:01:26 -0500

    use 1.12.2

--- build-external.sh
+++ build-external.sh
@@ -2,6 +2,6 @@
 mkdir ext
 cd ext
 curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar" -o BuildTools.jar
-java -jar BuildTools.jar
+java -jar BuildTools.jar --rev 1.12.2
 cd ..
 cp ext/spigot-1.12.2.jar templates/1_12_R1.jar
